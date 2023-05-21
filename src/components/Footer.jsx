f321989500a02aeca95cd22babc4cca7204f66c7
Av1Dev <avirevah32@gmail.com>
2023-05-21 19:31:52 +0300

    last update small phone fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,58 @@
-import React from 'react'
-import '../styles/footer.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../styles/footer.css";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
     <div className="footer">
       <div className="dev-credit">
-        <span>Developed by <a href = "mailto: avirevah32@gmail.com">Avi Revah</a> - Web Devloper ©</span>
-        </div>
+        <span>
+          Developed by <a href="mailto: avirevah32@gmail.com">Avi Revah</a> -
+          Web Devloper ©
+        </span>
+      </div>
       <div className="footer_wrapper">
         <div className="col">
           <ul>
             <li id="footer_title">אודות</li>
-            <Link to="/about"><li className="footer_item">אודות החברה</li></Link>
-            <Link to="/about"><li className="footer_item">צוות</li></Link>
+            <Link to="/about">
+              <li className="footer_item">אודות החברה</li>
+            </Link>
+            <Link to="/about">
+              <li className="footer_item">צוות</li>
+            </Link>
           </ul>
         </div>
         <div className="col">
           <ul>
             <li id="footer_title">שירותים</li>
-            <Link to="/about"><li className="footer_item">מוקדי מכירות</li></Link>
-            <Link to="/about"><li className="footer_item">מוקדי שימור</li></Link>
-            <Link to="/about"><li className="footer_item">מוקדי שירות</li></Link>
-            <Link to="/about"><li className="footer_item">סוכני שטח</li></Link>
-            <Link to="/jobs"><li className="footer_item">משרות</li></Link>
+            <Link to="/about">
+              <li className="footer_item">מוקדי מכירות</li>
+            </Link>
+            <Link to="/about">
+              <li className="footer_item">מוקדי שימור</li>
+            </Link>
+            <Link to="/about">
+              <li className="footer_item">מוקדי שירות</li>
+            </Link>
+            <Link to="/about">
+              <li className="footer_item">סוכני שטח</li>
+            </Link>
+            <Link to="/jobs">
+              <li className="footer_item">משרות</li>
+            </Link>
           </ul>
         </div>
         <div className="col">
           <ul>
-            <li className="footer_item" id="footer_title">צור קשר</li>
-            <li >המלאכה 23, ראש העין</li>
+            <li className="footer_item" id="footer_title">
+              צור קשר
+            </li>
+            <li>המלאכה 23, ראש העין</li>
             <li ><a href="tel:+972-54815117">054-4815117</a></li>
-            <li >Gius@infocall.co.il</li>
-            <li >מחלקה פיננסית</li>
-            <li >Finance@infocall.co.il</li>
+            <li>Gius@infocall.co.il</li>
+            <li>מחלקה פיננסית</li>
+            <li>Finance@infocall.co.il</li>
           </ul>
         </div>
         <div className="col">
@@ -46,5 +65,5 @@ export const Footer = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/pages/contact/ContactLocations.jsx
+++ src/pages/contact/ContactLocations.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import '../../styles/contact/contactLocations.css'
+import React from "react";
+import "../../styles/contact/contactLocations.css";
 
 export const ContactLocations = () => {
   return (
@@ -10,32 +10,30 @@ export const ContactLocations = () => {
           <div className="location-title">ראש העין</div>
           <div className="location-address">המלאכה, 23 - מטה</div>
           {/* <div className="location-address">מטה</div> */}
-          <div className="location-p-number">054-7714892 </div>
+          <div className="location-p-number">053-5328882 </div>
         </div>
 
         <div className="location">
-         <i class="fas fa-map-marker-alt"></i>
-         <div className="location-title">בני ברק</div>
-         <div className="location-address">רבי עקיבא, 31</div>
-         <div className="location-p-number">054-7714892 </div>
-        </div>
-
-        
-        <div className="location">
-         <i class="fas fa-map-marker-alt"></i>
-         <div className="location-title">ירושלים</div>
-         <div className="location-address">כנפי נשרים, 35</div>
-         <div className="location-p-number">054-7714892 </div>
+          <i class="fas fa-map-marker-alt"></i>
+          <div className="location-title">בני ברק</div>
+          <div className="location-address">רבי עקיבא, 31</div>
+          <div className="location-p-number">053-5328882 </div>
         </div>
 
         <div className="location">
-         <i class="fas fa-map-marker-alt"></i>
-         <div className="location-title">אשדוד</div>
-         <div className="location-address">האורגים, 11</div>
-         <div className="location-p-number">054-7714892 </div>
+          <i class="fas fa-map-marker-alt"></i>
+          <div className="location-title">ירושלים</div>
+          <div className="location-address">כנפי נשרים, 35</div>
+          <div className="location-p-number">053-5328882</div>
+        </div>
+
+        <div className="location">
+          <i class="fas fa-map-marker-alt"></i>
+          <div className="location-title">אשדוד</div>
+          <div className="location-address">האורגים, 11</div>
+          <div className="location-p-number">053-5328882</div>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
